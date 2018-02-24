3412434402df2696c4e919faf2f2d0be93c1c4a1
Jim Doty <jdoty@pivotal.io>
2018-02-23 18:33:21 -0700

    Playing around with module structure
    Not sure if using librarian-puppet to manage the downloads, and then
    commiting the conventions modules to git is the best idea.  First runs
    didn't explode, so lets get some meat to the modules and then revisit
    the organization topic.

---
+++ puppet/modules/conventions/manifests/team.pp
@@ -0,0 +1,6 @@
+class conventions::team {
+  ### - for now this will be hardcoded, but this section begs for composability
+  ## -> the git pairs file
+  ## -> [Customize bash-it](https://github.com/Bash-it/bash-it#custom-scripts-aliases-themes-and-functions)
+  ## -> ace-gpdb and stuff
+}
