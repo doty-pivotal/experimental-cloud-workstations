95938c68ea268da05a7d95e991db97a587812916
Jim Doty <jdoty@pivotal.io>
2018-03-01 10:00:06 -0700

    Group things we are punting on for now

--- bootstrap/workstation-setup/scripts/common/configuration-bash.sh
+++ bootstrap/workstation-setup/scripts/common/configuration-bash.sh
@@ -1,11 +1,9 @@
-echo
-echo "Configuring bash with bash-it"
 BOOTSTRAP_DIR=/vagrant/bootstrap/workstation-setup
 VAGRANT_HOME=/home/vagrant
-# brew install grc # Round 1 Punt 
-# brew install rbenv # Round 1 Punt 
+
+echo
+echo "Configuring bash with bash-it"
 cp $BOOTSTRAP_DIR/files/dircolors.ansi-dark $VAGRANT_HOME/.dircolors
-# cp $BOOTSTRAP_DIR/files/.inputrc $VAGRANT_HOME/.inputrc # Round 1 Punt
 rm -rf $VAGRANT_HOME/.bash_it
 git clone https://github.com/Bash-it/bash-it.git $VAGRANT_HOME/.bash_it
 cp $BOOTSTRAP_DIR/files/add_user_initials_to_git_prompt_info.bash $VAGRANT_HOME/.bash_it/custom
@@ -14,8 +12,13 @@ $VAGRANT_HOME/.bash_it/install.sh
 source $VAGRANT_HOME/.bash_profile
 bash-it enable completion git
 bash-it enable plugin ssh
-# bash-it enable plugin rbenv
 bash-it enable completion ssh
+
+## Punt for now - if you are missing something look here first
+# brew install grc # Round 1 Punt 
+# brew install rbenv # Round 1 Punt 
+# cp $BOOTSTRAP_DIR/files/.inputrc $VAGRANT_HOME/.inputrc # Round 1 Punt
+# bash-it enable plugin rbenv
 
 # echo
 # echo "Configuring direnv with bash-it"
