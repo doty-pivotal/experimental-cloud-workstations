dfbab15db6d891e540f27ea6f83efe745c17f6cf
Jim Doty <jdoty@pivotal.io>
2018-10-23 11:59:21 -0600

    Remove old style gpdb setup

--- products/gpdb/generate_env_scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source "/vagrant/workstation-setup/scripts/common/common.sh"
-print_script_header
-
-mkdir -p $HOME/scripts
-cp env_scripts/*.sh $HOME/scripts
-
-cat env_scripts/.bashrc >> $HOME/.bashrc
